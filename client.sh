bbc4638dfa4866843fd0a98088860e0cdcac431b
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-10 15:53:42 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -6,8 +6,8 @@ echo $IP
 hostname -I
 
 
-sudo chown -R postgres:postgres /var/lib/postgresql/10/
-sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
+#sudo chown -R postgres:postgres /var/lib/postgresql/10/
+#sudo chmod -R u=rwX,go= /var/lib/postgresql/10/
 
 # Restart so that all new config is loaded:
 sudo service postgresql restart
