37f934315498a87296fe58836b0caa46e66ded77
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-15 12:23:58 +0530

    Update vertex-container-build-with-postgress.sh

--- vertex-container-build-with-postgress.sh
+++ vertex-container-build-with-postgress.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib -y
-psql -U postgres -h 127.0.0.1 -c "create user test with password 'test';"
-psql -h 127.0.0.1 -c 'create database test owner test;' -U postgres
-psql -h 127.0.0.1 -U test -d test -c '\dt'
+psql -U postgres -h 127.0.0.1:5432 -c "create user test with password 'test';"
+psql -h 127.0.0.1:5432 -c 'create database test owner test;' -U postgres
+psql -h 127.0.0.1:5432 -U test -d test -c '\dt'
