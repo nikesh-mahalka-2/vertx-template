026e554d18f69b8af13197e05d5195ebaf0e2133
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-15 12:31:59 +0530

    Update vertex-container-build-with-postgress.sh

--- vertex-container-build-with-postgress.sh
+++ vertex-container-build-with-postgress.sh
@@ -8,6 +8,6 @@ sleep 10
 
 service postgresql status
 
-psql -U postgres -h 127.0.0.1 -c "create user test with password 'test';"
-psql -h 127.0.0.1 -c 'create database test owner test;' -U postgres
+psql -u postgres -h 127.0.0.1 -c "create user test with password 'test';"
+psql -h 127.0.0.1 -c 'create database test owner test;' -u postgres
 psql -h 127.0.0.1 -U test -d test -c '\dt'
