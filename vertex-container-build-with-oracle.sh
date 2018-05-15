9fa05ca6f7114598cfac34fa401ac64d12a3fbe5
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-16 00:37:23 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo apt-get update
+cp settings-security.xml ~/.m2/settings-security.xml
+
+mvn -encrypt-password test
 
 
-
-mvn -DskipTests -Ppostgres clean package
-java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run
+#mvn -DskipTests -Ppostgres clean package
+#java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run
