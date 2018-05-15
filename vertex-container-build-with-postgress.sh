4a986491c458a8f04c8ce473548e4a4d9cf03356
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-15 13:05:38 +0530

    Update vertex-container-build-with-postgress.sh

--- vertex-container-build-with-postgress.sh
+++ vertex-container-build-with-postgress.sh
@@ -7,5 +7,7 @@ service postgresql status
 sudo -i -u postgres psql -U postgres  -c "create user test with password 'test';"
 sudo -i -u postgres psql -c 'create database test owner test;' -U postgres
 PGPASSWORD=test psql  -U test -d test -h localhost -c '\dt'
+mvn -DskipTests -Ppostgres clean package
+java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run
 
 
