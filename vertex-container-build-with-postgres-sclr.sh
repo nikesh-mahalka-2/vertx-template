ee2b12d66bb2d374d56c63270b56113c9acd9286
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-17 15:14:46 +0530

    Update vertex-container-build-with-postgres-sclr.sh

--- vertex-container-build-with-postgres-sclr.sh
+++ vertex-container-build-with-postgres-sclr.sh
@@ -7,7 +7,7 @@ service postgresql start
 service postgresql status
 sudo -i -u postgres psql -U postgres  -c "create user test with password 'test';"
 sudo -i -u postgres psql -c 'create database test owner test;' -U postgres
-PGPASSWORD=test psql  -U test -d test -h localhost -c '\dt'
+
 mvn -DskipTests -Ppostgres clean package
 
 curl -sSL https://download.sourceclear.com/ci.sh | sh
