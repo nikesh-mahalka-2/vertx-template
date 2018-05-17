369cb7a57a7e2cd105642cdf4918ea3cf9e8259a
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-17 16:26:50 +0530

    Update vertex-container-build-with-postgres-sclr.sh

--- vertex-container-build-with-postgres-sclr.sh
+++ vertex-container-build-with-postgres-sclr.sh
@@ -7,11 +7,11 @@ service postgresql start
 service postgresql status
 sudo -i -u postgres psql -U postgres  -c "create user test with password 'test';"
 sudo -i -u postgres psql -c 'create database test owner test;' -U postgres
-sudo -i -u postgres psql -c '\dt' -U postgres
+PGPASSWORD=test psql  -U test -d test -h localhost -c '\dt'
 mvn -DskipTests -Ppostgres clean package
 
 curl -sSL https://download.sourceclear.com/ci.sh | sh
 
 nohup java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run 2>&1 &
 sleep 10
-sudo -i -u postgres psql -c '\dt' -U postgres
+PGPASSWORD=test psql  -U test -d test -h localhost -c '\dt'
