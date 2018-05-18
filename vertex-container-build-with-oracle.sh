50f1386fbb86219665572d86e1c39b86e50fc2da
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-18 19:22:29 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "print IP"
 hostname -I
+
+sudo docker run -d -it --name vertx-oracle-db alpine
+ORACLE_DB_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vertx-oracle-db)"
+
+echo "container IP"
+echo $ORACLE_DB_IP
+
+echo "network interface"
+ifconfig
