308a266a13fb0220700f2f8f835d62f4ff01cb63
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-18 14:16:33 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo mkdir -p /root/.m2/
-sudo cp ./settings-security.xml /root/.m2/
-mvn -DskipTests -Poracle package
+#sudo mkdir -p /root/.m2/
+#sudo cp ./settings-security.xml /root/.m2/
+#mvn -DskipTests -Poracle package
 #mvn -DskipTests -Poracle clean package
 
 #mvn -DskipTests -Ppostgres clean package
 #java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run
+docker login
