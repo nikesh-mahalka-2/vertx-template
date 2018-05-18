978db501c2c8bb592f3ca7709cc37abb5cfc48cd
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-18 15:18:31 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update
+#sudo apt-get update
 #sudo mkdir -p /root/.m2/
 #sudo cp ./settings-security.xml /root/.m2/
 #mvn -DskipTests -Poracle package
@@ -15,7 +15,7 @@ sudo apt-get update
 #sudo docker login -u nike1niec -p $DOCKER_LOGIN
 #sudo docker run -d -it --name vertx-oracle-db -P store/oracle/database-enterprise:12.2.0.1-slim
 docker run -d -it --name test-ubuntu ubuntu bash
-myIP=docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' test-ubuntu
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' test-ubuntu
 echo $myIP
 #mvn -DskipTests clean package
 
