f9e1814bcd0d87560748e44f0a87368b68c06c19
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-18 14:54:13 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -7,7 +7,12 @@ sudo apt-get update
 
 #mvn -DskipTests -Ppostgres clean package
 #java -jar target/vertx-template-1.0-SNAPSHOT.jar create-database run
-sudo docker login -u nike1niec -p $DOCKER_LOGIN
-sudo docker run -d -it --name vertx-oracle-db -P store/oracle/database-enterprise:12.2.0.1-slim
-sudo docker ps
-sudo docker images
+
+
+
+gsutil cp gs://delete-oracle-jdbc-jar/ojdbc7.jar .
+#sudo docker login -u nike1niec -p $DOCKER_LOGIN
+#sudo docker run -d -it --name vertx-oracle-db -P store/oracle/database-enterprise:12.2.0.1-slim
+
+
+
