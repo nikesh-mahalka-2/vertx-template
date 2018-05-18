9bc268e477aa07377f24b1d50f4d103ab7800a66
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-18 15:17:21 +0530

    Update vertex-container-build-with-oracle.sh

--- vertex-container-build-with-oracle.sh
+++ vertex-container-build-with-oracle.sh
@@ -10,9 +10,14 @@ sudo apt-get update
 
 
 
+
+#mvn install:install-file -Dfile=./ojdbc7.jar -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar
 #sudo docker login -u nike1niec -p $DOCKER_LOGIN
 #sudo docker run -d -it --name vertx-oracle-db -P store/oracle/database-enterprise:12.2.0.1-slim
-mvn install:install-file -Dfile=./ojdbc7.jar -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar
+docker run -d -it --name test-ubuntu ubuntu bash
+myIP=docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' test-ubuntu
+echo $myIP
+#mvn -DskipTests clean package
 
 
 
