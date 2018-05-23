f70e4d2bd98c352a1161193960886b659abc73ac
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-23 18:37:59 +0530

    Update wait-for-oracle.sh

--- wait-for-oracle.sh
+++ wait-for-oracle.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-until tnsping workspace_dbserver_1; do
+until sqlplus sys/Oradoc_db1@dbserver:1521/ORCLCDB.localdomain as sysdba; do
   >&2 echo "Oracle is unavailable - sleeping"
   sleep 30
 done
 
 >&2 echo "Oracle is up - executing command"
-exec "/bin/bash ls"
