c7f6eb3a195a658ede54bd04ce40c41eddbd11cc
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-23 17:04:20 +0530

    Update wait-for-oracle.sh

--- wait-for-oracle.sh
+++ wait-for-oracle.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-until java -Ddatabase.url=jdbc:oracle:thin:@workspace_dbserver_1:1521/ORCLCDB.localdomain -Djava.security.egd=file:/dev/./urandom -jar ./target/vertx-*-SNAPSHOT.jar create-database run; do
+until tnsping workspace_dbserver_1; do
   >&2 echo "Oracle is unavailable - sleeping"
   sleep 30
 done
 
 >&2 echo "Oracle is up - executing command"
 exec "/bin/bash ls"
-
