c85b32fc0be969c3065d2d8e352e50320643a90a
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-24 11:19:38 +0530

    Update docker-compose-testing.sh

--- docker-compose-testing.sh
+++ docker-compose-testing.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-echo "docker login -u nike1niec -p $DOCKER_LOGIN"
+echo "docker login"
 docker login -u nike1niec -p $DOCKER_LOGIN
-docker-compose up
+docker-compose up -d dbserver
+
+DB_HEALTH=""
+while [ "${DB_HEALTH}" != "\"healthy\"" ]
+do
+  DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' dbserver)"
+  sleep 5
+  echo $DB_HEALTH
+done
+
+docker-compose up appserver
