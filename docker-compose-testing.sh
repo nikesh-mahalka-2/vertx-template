3dcc939682d2523a51a771746eac11e96b2977e6
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-24 22:14:52 +0530

    Update docker-compose-testing.sh

--- docker-compose-testing.sh
+++ docker-compose-testing.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 echo "docker login"
 docker login -u nike1niec -p $DOCKER_LOGIN
+
 echo "docker-compose up -d dbserver" 
 docker-compose up -d dbserver
 
 echo "checking database health to be healthy"
+
 DB_HEALTH=""
 while [ "${DB_HEALTH}" != "\"healthy\"" ]
 do
   DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' dbserver)"
-  sleep 10
+  sleep 20
   echo $DB_HEALTH
 done
 
 echo "running docker-compose up appserver"
-docker-compose up appserver
+docker-compose up -d appserver
+
+sudo docker exec dbserver /workspace/check_oracle_table.sh
 
 docker-compose down
+
+
+
+
+
+
